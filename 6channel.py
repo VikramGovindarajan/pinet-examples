9803ac64e6f7e2cbe8164ea2570aad187d3cd596
VikramGovindarajan <vickymailbuddy@gmail.com>
2023-07-10 18:56:09 +0530

    feedback calculations for first zone added in 6channel model

--- 6channel.py
+++ 6channel.py
@@ -80,19 +80,19 @@ while i<=5:
         d=str(i+1)+str(l+1)
         if l==0:
             p.append(d+str(3))
-            g=HTcomp.HSlab("hslab"+d,"pipe"+d,"pipe",[fun1],"snode"+d,"hflux",0.0,FALAB[i],nlayers=3)
+            g=HTcomp.HSlab("hslab"+d,"snode"+d,"hflux",0.0,"pipe"+d,"pipe",[fun1],FALAB[i],nlayers=3)
             g.add_layer(3.81E-4,0.0203,2,FALABI[i],'SS23','User')
             g.add_layer(1.4E-4,0.0203,2,GIFALAB[i],'gap23','User')
             p[i*3+l]=g.add_layer(0.0024,0.0203,2,GIFALAB[i],'MOX23','User',heat_input=HILAB[i])
         elif l==1:
             p.append(d+str(3))
-            g=HTcomp.HSlab("hslab"+d,"pipe"+d,"pipe",[fun1],"snode"+d,"hflux",0.0,FAAC[i],nlayers=3)
+            g=HTcomp.HSlab("hslab"+d,"snode"+d,"hflux",0.0,"pipe"+d,"pipe",[fun1],FAAC[i],nlayers=3)
             g.add_layer(3.81E-4,0.9144,2,FAACI[i],'SS23','User')
             g.add_layer(7.E-5,0.9144,2,GIFAAC[i],'gap23','User')
             p[i*3+l]=g.add_layer(0.00247,0.9144,2,GIFAAC[i],'MOX23','User',10,heat_input=HIAC[i],AFF=AFF[i])
         elif l==2:
             p.append(d+str(3))
-            g=HTcomp.HSlab("hslab"+d,"pipe"+d,"pipe",[fun1],"snode"+d,"hflux",0.0,FAUAB[i],nlayers=3)
+            g=HTcomp.HSlab("hslab"+d,"snode"+d,"hflux",0.0,"pipe"+d,"pipe",[fun1],FAUAB[i],nlayers=3)
             g.add_layer(3.81E-4,0.0203,2,FAUABI[i],'SS23','User')
             g.add_layer(1.4E-4,0.0203,2,GIFAUAB[i],'gap23','User')
             p[i*3+l]=g.add_layer(0.0024,0.0203,2,GIFAUAB[i],'MOX23','User',heat_input=HIUAB[i])
@@ -142,92 +142,262 @@ from PINET import scheduler
 scheduler.delt = 1.
 scheduler.etime = 900    
 
-from rpdat import *
 
-# reading components
-from PINET.project import get_comp
-# radial zone1
-DHSNZ1LAB = get_comp("hslab11")
-DHSNZ1AC  = get_comp("hslab12")
-DHSNZ1UAB = get_comp("hslab13")
-PipeNZ1LAB = get_comp("pipe11")
-PipeNZ1AC  = get_comp("pipe12")
-PipeNZ1UAB = get_comp("pipe13")
+def pk1(time,delt):
 
-DHSNZ2LAB = get_comp("hslab21")
-DHSNZ2AC  = get_comp("hslab22")
-DHSNZ2UAB = get_comp("hslab23")
-PipeNZ2LAB = get_comp("pipe21")
-PipeNZ2AC  = get_comp("pipe22")
-PipeNZ2UAB = get_comp("pipe23")
-DHSNZ3LAB = get_comp("hslab31")
-DHSNZ3AC  = get_comp("hslab32")
-DHSNZ3UAB = get_comp("hslab33")
-PipeNZ3LAB = get_comp("pipe31")
-PipeNZ3AC  = get_comp("pipe32")
-PipeNZ3UAB = get_comp("pipe33")
-DHSNZ4LAB = get_comp("hslab41")
-DHSNZ4AC  = get_comp("hslab42")
-DHSNZ4UAB = get_comp("hslab43")
-PipeNZ4LAB = get_comp("pipe41")
-PipeNZ4AC  = get_comp("pipe42")
-PipeNZ4UAB = get_comp("pipe43")
-DHSNZ5LAB = get_comp("hslab51")
-DHSNZ5AC  = get_comp("hslab52")
-DHSNZ5UAB = get_comp("hslab53")
-PipeNZ5LAB = get_comp("pipe51")
-PipeNZ5AC  = get_comp("pipe52")
-PipeNZ5UAB = get_comp("pipe53")
-DHSNZ6LAB = get_comp("hslab61")
-DHSNZ6AC  = get_comp("hslab62")
-DHSNZ6UAB = get_comp("hslab63")
-PipeNZ6LAB = get_comp("pipe61")
-PipeNZ6AC  = get_comp("pipe62")
-PipeNZ6UAB = get_comp("pipe63")
+    # reading components
+    from PINET.project import get_comp
+    # radial zone1
+    DHSNZ1LAB = get_comp("hslab11")
+    DHSNZ1AC  = get_comp("hslab12")
+    DHSNZ1UAB = get_comp("hslab13")
+    PipeNZ1LAB = get_comp("pipe11")
+    PipeNZ1AC  = get_comp("pipe12")
+    PipeNZ1UAB = get_comp("pipe13")
+
+    DHSNZ2LAB = get_comp("hslab21")
+    DHSNZ2AC  = get_comp("hslab22")
+    DHSNZ2UAB = get_comp("hslab23")
+    PipeNZ2LAB = get_comp("pipe21")
+    PipeNZ2AC  = get_comp("pipe22")
+    PipeNZ2UAB = get_comp("pipe23")
+    DHSNZ3LAB = get_comp("hslab31")
+    DHSNZ3AC  = get_comp("hslab32")
+    DHSNZ3UAB = get_comp("hslab33")
+    PipeNZ3LAB = get_comp("pipe31")
+    PipeNZ3AC  = get_comp("pipe32")
+    PipeNZ3UAB = get_comp("pipe33")
+    DHSNZ4LAB = get_comp("hslab41")
+    DHSNZ4AC  = get_comp("hslab42")
+    DHSNZ4UAB = get_comp("hslab43")
+    PipeNZ4LAB = get_comp("pipe41")
+    PipeNZ4AC  = get_comp("pipe42")
+    PipeNZ4UAB = get_comp("pipe43")
+    DHSNZ5LAB = get_comp("hslab51")
+    DHSNZ5AC  = get_comp("hslab52")
+    DHSNZ5UAB = get_comp("hslab53")
+    PipeNZ5LAB = get_comp("pipe51")
+    PipeNZ5AC  = get_comp("pipe52")
+    PipeNZ5UAB = get_comp("pipe53")
+    DHSNZ6LAB = get_comp("hslab61")
+    DHSNZ6AC  = get_comp("hslab62")
+    DHSNZ6UAB = get_comp("hslab63")
+    PipeNZ6LAB = get_comp("pipe61")
+    PipeNZ6AC  = get_comp("pipe62")
+    PipeNZ6UAB = get_comp("pipe63")
+
+    from rpdat import TOREF,TFR
+    from rpdat import TC1AC1  ,TC1LAB1 ,TC1UAB1 ,TC2AC1  ,TC2LAB1 ,TC2UAB1 ,TC3AC1,TC3LAB1 ,TC3UAB1 ,TCDAC1,TCDLAB1 ,TCDUAB1 ,TCD2AC1 ,TCD2LAB1,TCD2UAB1
+    from rpdat import TC1AC2  ,TC1LAB2 ,TC1UAB2 ,TC2AC2  ,TC2LAB2 ,TC2UAB2 ,TC3AC2,TC3LAB2 ,TC3UAB2 ,TCDAC2,TCDLAB2 ,TCDUAB2 ,TCD2AC2 ,TCD2LAB2,TCD2UAB2
+    from rpdat import TC1AC3  ,TC1LAB3 ,TC1UAB3 ,TC2AC3  ,TC2LAB3 ,TC2UAB3 ,TC3AC3  ,TC3LAB3 ,TC3UAB3 ,TCDAC3  ,TCDLAB3 ,TCDUAB3 ,TCD2AC3 ,TCD2LAB3,TCD2UAB3
+    from rpdat import TC1AC4  ,TC1LAB4 ,TC1UAB4 ,TC2AC4  ,TC2LAB4 ,TC2UAB4 ,TC3AC4  ,TC3LAB4 ,TC3UAB4 ,TCDAC4  ,TCDLAB4 ,TCDUAB4 ,TCD2AC4 ,TCD2LAB4,TCD2UAB4
+    from rpdat import TC1AC5  ,TC1LAB5 ,TC1UAB5 ,TC2AC5  ,TC2LAB5 ,TC2UAB5 ,TC3AC5  ,TC3LAB5 ,TC3UAB5 ,TCDAC5  ,TCDLAB5 ,TCDUAB5 ,TCD2AC5 ,TCD2LAB5,TCD2UAB5
+    from rpdat import TC1AC6  ,TC1LAB6 ,TC1UAB6 ,TC2AC6  ,TC2LAB6 ,TC2UAB6 ,TC3AC6  ,TC3LAB6 ,TC3UAB6 ,TCDAC6  ,TCDLAB6 ,TCDUAB6 ,TCD2AC6 ,TCD2LAB6,TCD2UAB6
 
 
-# calculate TRFL, TRCL, TRNA, TRDOP
-TRFL  = 0.0
-TRCL  = 0.0
-TRNA  = 0.0
-TRDOP = 0.0
+    # calculate TRFL, TRCL, TRNA, TRDOP
+    TRFL  = 0.0
+    TRCL  = 0.0
+    TRNA  = 0.0
+    TRDOP = 0.0
 
-# 1ST ZONE
-RFL  = 0.0
-RCL  = 0.0
-RNA  = 0.0
-RDOP = 0.0
+    # 1ST ZONE
+    RFL  = 0.0
+    RCL  = 0.0
+    RNA  = 0.0
+    RDOP = 0.0
 
-# for (K=1;K<=10;K++) { #pending automate
-# # fuel region 
- # TNAAC = PipeNZ1AC.GetPropertyFromFullDisplayName("Downstream node, Increment "+K+".{Flow Node Results}Total temperature")
- # RNA = RNA + TC3AC1[K-1]*(TNAAC-TOREF);
+    K = 0
+    for K in range(10): #pending automate
+        # fuel region 
+        TNAAC = PipeNZ1AC.faces[K].dnode.ttemp_gues
+        RNA = RNA + TC3AC1[K-1]*(TNAAC-TOREF)
+        
+        TDA3 = DHSNZ1AC.layers[0].nodes[10+K].temp_gues
+        TDA4 = DHSNZ1AC.layers[1].ifaces[10+K].temp_gues
+        TDA6=0.5*(TDA3+TDA4)
+        QDNA=TDA6/TOREF
+        if (TDA6<=1000.0 and TDA6 > 473.0):#pending automate
+            RDOP = RDOP + TCDAC1[K-1]*math.log(QDNA)
+        elif (TDA6 > 1000.0 and TDA6 <= 2000.0):
+            RDOP = RDOP + TCD2AC1[K-1]*math.log(QDNA)
+        else:
+            print("warning: Doppler out of range. Time = " + time)
+            sys.exit()
 
- # IPS.Properties.Double TDA3 = DHSNZ1AC.GetPropertyFromFullDisplayName("Upstream node, HT element direction 2, Cross direction "+K+".{Solid Node Results}Temperature") as IPS.Properties.Double;
- # IPS.Properties.Double TDA4 = DHSNZ1AC.GetPropertyFromFullDisplayName("Sub-element, HT element direction 2, Cross direction "+K+".{Generic}Temperature") as IPS.Properties.Double;
-  # TDA6=0.5*(TDA3+TDA4);
-  # QDNA=TDA6/TOREF;
-  # if (TDA6<=1000.0 && TDA6 > 473.0) { //pending automate
-      # RDOP = RDOP + TCDAC1[K-1]*Math.Log(QDNA);
-  # }
-  # else if (TDA6 > 1000.0 && TDA6 <= 2000.0){
-      # RDOP = RDOP + TCD2AC1[K-1]*Math.Log(QDNA);
-  # }
-  # else {
-      # IPS.Task.ConsoleSolverOutputProvider.GetConsoleOutputWindow().AddTextLine("warning: Doppler out of range. Time = " + Time);
-      # return;
-  # }
-  
+        TAC1 = DHSNZ1AC.layers[2].nodes[K].temp_gues
+        TAC2 = DHSNZ1AC.layers[2].nodes[K].eface.temp_gues
+        TAC=0.5*(TAC1+TAC2)
+        RCL = RCL + TC2AC1[K-1]*(TAC-TOREF)
 
- # IPS.Properties.Double TAC1 = DHSNZ1AC.GetPropertyFromFullDisplayName("Upstream node, HT element direction 5, Cross direction "+K+".{Solid Node Results}Temperature") as IPS.Properties.Double;
- # IPS.Properties.Double TAC2 = DHSNZ1AC.GetPropertyFromFullDisplayName("Sub-element, HT element direction 5, Cross direction "+K+".{Generic}Temperature") as IPS.Properties.Double;
- # TAC=0.5*(TAC1+TAC2);
- # RCL = RCL + TC2AC1[K-1]*(TAC-TOREF);
+        IFR = 0
+        if (IFR==1): # fuel stuck to clad
+            RFL = RFL + TC1AC1[K-1]*(TAC-TOREF)
+        elif (IFR==0): # fuel free to expand
+            RFL = RFL + TC1AC1[K-1]*(TDA6-TOREF)
 
-    # // if (IFR==1) { //fuel stuck to clad
-        # // RFL = RFL + TC1AC1[K-1]*(TAC-TOREF);
-    # // }
-    # // else if (IFR==0) { //fuel free to expand
-    # RFL = RFL + TC1AC1[K-1]*(TDA6-TOREF);
-    # // }
-# }
+    K = 0
+    # bottom axial blanket region
+    TLAB1 = DHSNZ1LAB.layers[2].nodes[K].temp_gues
+    TLAB2 = DHSNZ1LAB.layers[2].nodes[K].eface.temp_gues
+    TLAB=0.5*(TLAB1+TLAB2)
+    RCL = RCL + TC2LAB1*(TLAB-TOREF)
+
+    TNALAB = PipeNZ1LAB.dnode.ttemp_gues
+    RNA = RNA + TC3LAB1*(TNALAB-TOREF)
+
+    TDL3 = DHSNZ1LAB.layers[0].nodes[1+K].temp_gues
+    TDL4 = DHSNZ1LAB.layers[1].ifaces[1+K].temp_gues
+    TDL6=0.5*(TDL3+TDL4)
+    QDNL=TDL6/TOREF
+    if (TDL6<=1000.0 and TDL6 > 473.0):
+        RDOP = RDOP + TCDLAB1*math.log(QDNL)
+    elif (TDL6 > 1000.0 and TDL6 <= 2000.0):
+        RDOP = RDOP + TCD2LAB1*math.log(QDNL)
+    else:
+      print("warning: Doppler out of range. time = " + Time)
+      sys.exit()
+
+    if (IFR==1): #fuel stuck to clad
+        RFL = RFL + TC1LAB1*(TLAB-TOREF)
+    elif (IFR==0): #fuel free to expand
+        RFL = RFL + TC1LAB1*(TDL6-TOREF)
+
+    # upper axial blanket
+    TUAB1 = DHSNZ1UAB.layers[2].nodes[K].temp_gues
+    TUAB2 = DHSNZ1UAB.layers[2].nodes[K].eface.temp_gues
+    TUAB = 0.5*(TUAB1+TUAB2)
+    RCL = RCL + TC2UAB1*(TUAB-TOREF)
+
+    TNAUAB = PipeNZ1UAB.dnode.ttemp_gues
+    RNA = RNA + TC3UAB1*(TNAUAB-TOREF)
+
+    TDU3 = DHSNZ1UAB.layers[0].nodes[1+K].temp_gues
+    TDU4 = DHSNZ1UAB.layers[1].ifaces[1+K].temp_gues
+    TDU6=0.5*(TDU3+TDU4)
+    QDNU=TDU6/TOREF
+    if (TDU6<=1000.0 and TDU6 > 473.0):
+        RDOP = RDOP + TCDUAB1*math.log(QDNU)
+    elif (TDU6 > 1000.0 and TDU6 <= 2000.0):
+        RDOP = RDOP + TCD2UAB1*math.log(QDNU)
+    else:
+        print("warning: Doppler out of range. time = " + Time)
+        sys.exit()
+
+    if (IFR==1): #fuel stuck to clad
+        RFL = RFL + TC1UAB1*(TUAB-TOREF)
+    elif (IFR==0): #fuel free to expand
+       RFL = RFL + TC1UAB1*(TDU6-TOREF)
+
+    TRCL = TRCL + RCL
+    TRFL = TRFL + RFL
+    TRNA = TRNA + RNA
+    TRDOP = TRDOP + RDOP
+
+    print ("flag1",TRNA,TRDOP,TRFL,TRCL)
+    sys.exit()
+
+    # // 2ND ZONE
+    # RFL=0.0;
+    # RCL=0.0;
+    # RNA=0.0;
+    # RDOP=0.0;
+
+    # for (K=1;K<=10;K++) {
+      # IPS.Properties.Double TAC1 = DHSNZ2AC.GetPropertyFromFullDisplayName("Upstream node, HT element direction 5, Cross direction "+K+".{Solid Node Results}Temperature") as IPS.Properties.Double;
+      # IPS.Properties.Double TAC2 = DHSNZ2AC.GetPropertyFromFullDisplayName("Sub-element, HT element direction 5, Cross direction "+K+".{Generic}Temperature") as IPS.Properties.Double;
+      # TAC=0.5*(TAC1+TAC2);
+      # RCL = RCL + TC2AC2[K-1]*(TAC-TOREF);
+      # IPS.Properties.Double TNAAC = PipeNZ2AC.GetPropertyFromFullDisplayName("Downstream node, Increment "+K+".{Flow Node Results}Total temperature") as IPS.Properties.Double;
+     # RNA = RNA + TC3AC2[K-1]*(TNAAC-TOREF);
+
+      # IPS.Properties.Double TDA3 = DHSNZ2AC.GetPropertyFromFullDisplayName("Upstream node, HT element direction 2, Cross direction "+K+".{Solid Node Results}Temperature") as IPS.Properties.Double;
+      # IPS.Properties.Double TDA4 = DHSNZ2AC.GetPropertyFromFullDisplayName("Sub-element, HT element direction 2, Cross direction "+K+".{Generic}Temperature") as IPS.Properties.Double;
+      # TDA6=0.5*(TDA3+TDA4);
+      # QDNA=TDA6/(TOREF);
+      # if (TDA6<=1000.0 && TDA6 > 473.0) {
+          # RDOP = RDOP + TCDAC2[K-1]*Math.Log(QDNA);
+      # }
+      # else if (TDA6 > 1000.0 && TDA6 <= 2000.0){
+          # RDOP = RDOP + TCD2AC2[K-1]*Math.Log(QDNA);
+      # }
+      # else {
+          # IPS.Task.ConsoleSolverOutputProvider.GetConsoleOutputWindow().AddTextLine("warning: Doppler out of range. time = "+ Time); 
+          # return;
+      # }
+     
+        # // if (IFR==1) { //fuel stuck to clad
+            # // RFL = RFL + TC1AC2[K-1]*(TAC-TOREF);
+        # // }
+        # // else if (IFR==0) { //fuel free to expand
+            # RFL = RFL + TC1AC2[K-1]*(TDA6-TOREF);
+        # // }
+
+    # }
+
+    # TLAB1 = DHSNZ2LAB.GetPropertyFromPropID("UN,5,-1,-1,-1.Temperature") as IPS.Properties.Double;
+    # TLAB2 = DHSNZ2LAB.GetPropertyFromPropID("SE,5,-1,-1,-1.Temperature") as IPS.Properties.Double;
+    # TLAB=0.5*(TLAB1+TLAB2);
+    # RCL = RCL + TC2LAB2*(TLAB-TOREF);
+
+    # TNALAB = PipeNZ2LAB.GetPropertyFromPropID("TotalTemperatureNode2") as IPS.Properties.Double;
+    # RNA = RNA + TC3LAB2*(TNALAB-TOREF);
+
+    # TDL3 = DHSNZ2LAB.GetPropertyFromPropID("UN,2,-1,-1,-1.Temperature") as IPS.Properties.Double;
+    # TDL4 = DHSNZ2LAB.GetPropertyFromPropID("SE,2,-1,-1,-1.Temperature") as IPS.Properties.Double;
+    # TDL6=0.5*(TDL3+TDL4);
+    # QDNL=TDL6/(TOREF);
+      # if (TDL6<=1000.0 && TDL6 > 473.0) {
+          # RDOP = RDOP + TCDLAB2*Math.Log(QDNL);
+      # }
+      # else if (TDL6 > 1000.0 && TDL6 <= 2000.0){
+          # RDOP = RDOP + TCD2LAB2*Math.Log(QDNL);
+      # }
+      # else {
+          # IPS.Task.ConsoleSolverOutputProvider.GetConsoleOutputWindow().AddTextLine("warning: Doppler out of range. time =" + Time); 
+          # return;
+      # }
+
+        # // if (IFR==1) { //fuel stuck to clad
+            # // RFL = RFL + TC1LAB2*(TLAB-TOREF);
+        # // }
+        # // else if (IFR==0) { //fuel free to expand
+            # RFL = RFL + TC1LAB2*(TDL6-TOREF);
+        # // }
+
+    # TUAB1 = DHSNZ2UAB.GetPropertyFromPropID("UN,5,-1,-1,-1.Temperature") as IPS.Properties.Double;
+    # TUAB2 = DHSNZ2UAB.GetPropertyFromPropID("SE,5,-1,-1,-1.Temperature") as IPS.Properties.Double;
+    # TUAB = 0.5*(TUAB1+TUAB2);
+    # RCL = RCL + TC2UAB2*(TUAB-TOREF);
+    # TNAUAB = PipeNZ2UAB.GetPropertyFromPropID("TotalTemperatureNode2") as IPS.Properties.Double;
+    # RNA = RNA + TC3UAB2*(TNAUAB-TOREF);
+
+    # TDU3 = DHSNZ2UAB.GetPropertyFromPropID("UN,2,-1,-1,-1.Temperature") as IPS.Properties.Double;
+    # TDU4 = DHSNZ2UAB.GetPropertyFromPropID("SE,2,-1,-1,-1.Temperature") as IPS.Properties.Double;
+    # TDU6=0.5*(TDU3+TDU4);
+    # QDNU=TDU6/(TOREF);
+      # if (TDU6<=1000.0 && TDU6 > 473.0) {
+          # RDOP = RDOP + TCDUAB2*Math.Log(QDNU);
+      # }
+      # else if (TDU6 > 1000.0 && TDU6 <= 2000.0){
+          # RDOP = RDOP + TCD2UAB2*Math.Log(QDNU);
+      # }
+      # else {
+          # IPS.Task.ConsoleSolverOutputProvider.GetConsoleOutputWindow().AddTextLine("warning: Doppler out of range Time = "+Time);
+          # return;
+      # }
+
+        # // if (IFR==1) { //fuel stuck to clad
+            # // RFL = RFL + TC1UAB2*(TUAB-TOREF);
+        # // }
+        # // else if (IFR==0) { //fuel free to expand
+            # RFL = RFL + TC1UAB2*(TDU6-TOREF);
+        # // }
+
+
+    # TRCL.Value = TRCL.Value + RCL;
+    # TRFL.Value = TRFL.Value + RFL;
+    # TRNA.Value = TRNA.Value + RNA;
+    # TRDOP.Value = TRDOP.Value + RDOP;
+
+action_setup.Action(None,None,pk1)
