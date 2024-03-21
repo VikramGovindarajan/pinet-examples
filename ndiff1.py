f2e4be842b0b5577b4790109b10504b9c778c329
VikramGovindarajan <vickymailbuddy@gmail.com>
2024-03-21 11:44:45 +0530

    add F factor add convergence on lambda also

--- ndiff1.py
+++ ndiff1.py
@@ -11,6 +11,7 @@ nuEf = 2E2
 
 n = 3
 A = np.zeros((n,n))
+F = np.zeros((n,n))
 b = np.zeros(n)
 phi = np.zeros(n)
 X = np.zeros(n)
@@ -24,6 +25,9 @@ A[1,0] = -2*D
 A[1,1] = 6*D+Ea*h**2
 A[2,0] = -2*D
 A[2,2] = 2*D+Ea*h**2
+F[0,0] = nuEf
+F[1,1] = nuEf
+F[2,2] = nuEf
 
 phi[0] = 1
 phi[1] = 1
@@ -33,18 +37,17 @@ X[1] = 1
 X[2] = 1
 
 
-for i in range(20):
+for i in range(100):
 
-    b[0] = nuEf*phi[0]/lambd
-    b[1] = nuEf*phi[1]/lambd
-    b[2] = nuEf*phi[2]/lambd
+    b = np.dot(F,phi)/lambd
 
     phi1 = scipy.linalg.solve(A,b)
     lambd1 = lambd*np.inner(phi1,X)/np.inner(phi,X)
 
-    e = abs(np.linalg.norm(phi1)-np.linalg.norm(phi))
-    print (i,e)
-    if(e<1.E-5):
+    e1 = abs(np.linalg.norm(phi1)-np.linalg.norm(phi))
+    e2 = abs(lambd1-lambd)
+    print (i,e1,e2)
+    if(e1<1.E-7 and e2<1.E-5):
         break
 
     phi = phi1
@@ -56,31 +59,31 @@ else:
 print(phi)
 print(lambd)
 
-phi2d = np.zeros((2,2))
-phi2d[0,0] = phi[0]
-phi2d[0,1] = phi[1]
-phi2d[1,0] = phi[2]
-phi2d[1,1] = phi[0]
+# phi2d = np.zeros((2,2))
+# phi2d[0,0] = phi[0]
+# phi2d[0,1] = phi[1]
+# phi2d[1,0] = phi[2]
+# phi2d[1,1] = phi[0]
 
 
-from matplotlib.colors import BoundaryNorm
-from matplotlib.ticker import MaxNLocator
+# from matplotlib.colors import BoundaryNorm
+# from matplotlib.ticker import MaxNLocator
 
-x = np.linspace(0,5,3)
-y = np.linspace(0,5,3)
-x, y = np.meshgrid(x, y)
+# x = np.linspace(0,5,3)
+# y = np.linspace(0,5,3)
+# x, y = np.meshgrid(x, y)
 
-z = phi2d
+# z = phi2d
 
-levels = MaxNLocator(nbins=10).tick_values(0, z.max())
+# levels = MaxNLocator(nbins=10).tick_values(z.min(), z.max())
 
-cmap = plt.colormaps['coolwarm']
-norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
+# cmap = plt.colormaps['coolwarm']
+# norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
 
-fig, ax = plt.subplots()
+# fig, ax = plt.subplots()
 
-im = ax.pcolormesh(x, y, z, cmap=cmap, norm=norm)
-fig.colorbar(im, ax=ax)
-ax.set_title('flux distribution')
+# im = ax.pcolormesh(x, y, z, cmap=cmap, norm=norm)
+# fig.colorbar(im, ax=ax)
+# ax.set_title('flux distribution')
 
-plt.show()
+# plt.show()
